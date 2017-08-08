7b7c75ec3de8793d370935708ed168a386601124
Leticia Freire de Figueiredo <leticia.freire.de.figueiredo@cern.ch>
2017-08-08 20:31:37 +0200

    Calculating the first appearance

--- game.py
+++ game.py
@@ -36,10 +36,29 @@ for chap_death in data["Death Chapter"]:
 #taking the books each character is in
 books_appearance = []
 for i in range(0, len(data["GoT"])):
-	temp = [[data["GoT"][i]] + [data["CoK"][i]] + [data["GoT"][i]] + [data["SoS"][i]] + [data["FfC"][i]] + [data["DwD"][i]]]
+	temp = [[data["GoT"][i]] + [data["CoK"][i]] + [data["SoS"][i]] + [data["FfC"][i]] + [data["DwD"][i]]]
 	books_appearance = books_appearance + temp
 
+#checking if is ok
 print chapter_intro
 print book_death
 print death_chapter
 print books_appearance
+
+#calculating the life time of each character
+life_time = []
+#see every character in the data
+for character in range(0, len(books_appearance)):
+	first_appearance = False
+	for book in range(0, len(books_appearance[character])):
+		#taking the first appearance of each character
+		if books_appearance[character][book] == 1 and first_appearance == False:
+			first_appearance = True
+			#calculating his life time
+			#if the character appears in the first book, it needs to be older than the character that appears in the last book
+			#because of this, each book has weight, the first book has weight 5 and the last, 1
+			#the first appearance of each character has value depending on the book weight 
+			life_time = life_time + [(book+1)*(5-book)]
+			break
+
+print life_time
